141dd85f7a2566e6223b6a8729d81f8e45b194b7
Jay Nair <abhijaynair@icloud.com>
2024-06-18 18:33:27 -0400

    First Commit

---
+++ Roulette Watch App/CoinToss.swift
@@ -0,0 +1,18 @@
+//
+//  CoinToss.swift
+//  Roulette Watch App
+//
+//  Created by Abhijay Nair on 6/17/24.
+//
+
+import SwiftUI
+
+struct CoinToss: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinToss()
+}
---
+++ Roulette Watch App/RoulettePicker.swift
@@ -0,0 +1,18 @@
+//
+//  RoulettePicker.swift
+//  Roulette Watch App
+//
+//  Created by Abhijay Nair on 6/17/24.
+//
+
+import SwiftUI
+
+struct RoulettePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoulettePicker()
+}
